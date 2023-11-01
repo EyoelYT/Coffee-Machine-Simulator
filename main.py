bd2949e1c9c54c0d7a30de2f7e53533fada3a4fe
Eyoel Tesfu <eyoelytesfu@gmail.com>
2023-11-01 18:24:31 -0500

    Added the main loop

--- main.py
+++ main.py
@@ -36,3 +36,30 @@ resources = {
     "coffee": 100,
 }
 
+def main():
+    userInput = input("What would you like? (espresso/latte/cappuccino):").lower()
+    # check what customer wants
+    # check what customer entered
+    # check if resources are sufficient
+    if userInput not in {"espresso", "latte", "cappuccino", "report", "off"}:
+        print("Invalid input noticed. Please enter a valid option.")
+
+    while userInput != "off":
+        if userInput == "report":
+            printReport()
+        elif user_can_buy(userInput):
+            print("Transaction successful")
+            deductUsedProductIngredients(userInput)
+            print(f"Here is your {userInput}. Enjoy!")
+        else:
+            print("Money refunded.")
+
+        userInput = input("What would you like? (espresso/latte/cappuccino):").lower()
+
+
+main()
+
+
+
+
+
